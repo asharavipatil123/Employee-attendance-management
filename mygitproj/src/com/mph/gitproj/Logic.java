312a70b82a02f0edf0bdc1ee1e1a12e49890fd3f
pradheep2 <pradheepvasan20@gmail.com>
2022-01-04 13:54:13 +0530

    Added project

---
+++ mygitproj/src/com/mph/gitproj/Logic.java
@@ -0,0 +1,16 @@
+package com.mph.gitproj;
+
+import java.util.LinkedList;
+
+public class Logic {
+	public static void main(String args[]){  
+		  
+		  LinkedList<String> al=new LinkedList<String>();  
+		   al.add("Raj");  
+		   al.add("Jay");  
+		   al.add("Ram");  
+		   al.add("Ajay");  
+			 System.out.println("LinkedList : " + al);  
+		  
+		 } }	
+
